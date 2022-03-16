55b1f0654a40b5dad2fd5f0ec0a4dcb2b737c6de
Thanh Dat <99642423+SturdyStudent@users.noreply.github.com>
2022-03-16 12:08:20 +0700

    Add Routing

--- website-ve-mb/src/App.js
+++ website-ve-mb/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home/HomePage';
+import Payment from './pages/Payment/PaymentPage';
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/payment" element={<Payment/>}/>
+      </Routes>
+  </Router> 
   );
 }
 
--- website-ve-mb/src/pages/Home/HomePage.js
+++ website-ve-mb/src/pages/Home/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div>HomePages</div>
+    <div>HomePage</div>
   )
 }
 
