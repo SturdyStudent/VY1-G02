c598cd288f0b3478c50e9fd747916ec5c0fe9103
Thanh Dat <99642423+SturdyStudent@users.noreply.github.com>
2022-04-12 20:57:13 +0700

    Update server.js

--- server-database/server.js
+++ server-database/server.js
@@ -13,6 +13,6 @@
 // // set port, listen for requests
  const PORT = process.env.PORT || 3001;
  app.listen(PORT, () => {
-   console.log(`Server is running on port ${PORT}.`);
+   console.log(`Server is walking on port ${PORT}.`);
  });
 
