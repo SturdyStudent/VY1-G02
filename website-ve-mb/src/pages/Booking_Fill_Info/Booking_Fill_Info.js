9ae2b20e8216472bf2567383ce8e01500291d8f5
VuongTranKameru <0918051090t@gmail.com>
2022-04-17 14:06:04 +0700

    add something

--- website-ve-mb/src/pages/Booking_Fill_Info/Booking_Fill_Info.js
+++ website-ve-mb/src/pages/Booking_Fill_Info/Booking_Fill_Info.js
@@ -7,37 +7,41 @@ function Booking_Fill_Info() {
   return (
       <div>
           <Header/>
-        <div class="middle" >
+        <div class="booking" >
             <h1>Đặt chỗ của tôi</h1>
             <p style={{"marginTop":"-10px"}}>Điền thông tin và xem lại đặt chỗ.</p>
         
             <h2>Thông tin liên hệ</h2>
             <form>
-                <table id='table'>
-                    <tr id='tr'>
-                        <th class="titlelong">Thông tin liên hệ (nhận vé/phiếu thanh toán)</th>
-                        <th class="fixbttn"><button id='button'>Lưu</button></th>
+                <table class="bookinfo">
+                    <tr>
+                        <th class="booktitlelong">
+                            Thông tin liên hệ (nhận vé/phiếu thanh toán)
+                        </th>
+                        <th class="bookfix">
+                            <button class="bookfixbttn" id='button'>Lưu</button>
+                        </th>
                     </tr>
-                    <tr id='tr'>
+                    <tr>
                         <td>
                             <br/><label for="holh">Họ (vd: Nguyen)</label><br/>
                             <input type="text" id="holh" name="holh"/><br/>
                             <p class="note">như trên CMND (không dấu)</p>
                         </td>
-                        <td>
+                        <td align='right'>
                             <br/><label for="tenlh">Tên Đệm & Tên (vd: Thi An)</label><br/>
                             <input type="text" id="tenlh" name="tenlh"/><br/>
                             <p class="note">như trên CMND (không dấu)</p>
                         </td>
                     </tr>
-                    <tr id='tr'>
+                    <tr>
                         <td>
                             <label for="dtlh">Điện thoại di động</label><br/>
                             <input type="text" id="tenlh" name="dtlh"/><br/>
                             <p class="note">vd: +84 901234567 trong đó +84 là
                                 <br/> mã quốc gia và 901234567 là số di động</p>
                         </td>
-                        <td>
+                        <td align='right'>
                             <label for="maillh">Email</label><br/>
                             <input type="text" id="maillh" name="maillh"/><br/>
                             <p class="note">abc123@email.com</p>
@@ -45,14 +49,17 @@ function Booking_Fill_Info() {
                     </tr>
                 </table>
             </form>
-            <h2>Thông tin hành khách</h2>
+        
+        <h2>Thông tin hành khách</h2>
         <form>
-            <table id='table'>
-                <tr id='tr'>
-                    <th class="title">Người đặt 1</th>
-                    <th class="fixbttn"><button id='button'>Lưu</button></th>
+            <table class="bookinfo">
+                <tr>
+                    <th class="booktitle">Người đặt 1</th>
+                    <th class="bookfix">
+                        <button class="bookfixbttn" id='button'>Lưu</button>
+                    </th>
                 </tr>
-                <tr id='tr'>
+                <tr>
                     <td>
                         <br/><label for="xungnhat">Danh xưng</label>
                         <select id="xungnhat" name="xung">
@@ -63,25 +70,25 @@ function Booking_Fill_Info() {
                         </select>
                     </td>
                 </tr>
-                <tr id='tr'>
+                <tr>
                     <td>
                         <br/><label for="honhat">Họ (vd: Nguyen)</label><br/>
                         <input type="text" id="honhat" name="ho"/><br/>
                         <p class="note">như trên CMND (không dấu)</p>
                     </td>
-                    <td>
+                    <td align='right'>
                         <br/><label for="tennhat">Tên Đệm & Tên (vd: Thi An)</label><br/>
                         <input type="text" id="tennhat" name="ten"/><br/>
                         <p class="note">như trên CMND (không dấu)</p>
                     </td>
                 </tr>
-                <tr id='tr'>
+                <tr>
                     <td>
                         <label for="sinhnhat">Ngày sinh</label><br/>
                         <input type="text" id="sinhnhat" name="sinh"/><br/>
                         <p class="note">hành khách người lớn (trên 12 tuổi)</p>
                     </td>
-                    <td>
+                    <td align='right'>
                         <label for="quocnhat">Quốc tịch</label><br/>
                         <select  id="quocnhat" name="quoc">
                             <option value="null"></option>
@@ -94,7 +101,9 @@ function Booking_Fill_Info() {
             </table>
         </form>
         <br/><br/>
-        <a href="XemThongTinDatCho.html">Tiếp tục</a>
+        <div class="bookconfirm">
+            <a href="">Tiếp tục</a>
+        </div>
         </div>  
     </div>
   )
