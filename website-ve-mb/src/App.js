3dc74cd868b64e6029a179b0b5468930c351253d
SturdyStudent <thanhdat5101@gmail.com>
2022-04-17 19:49:17 +0700

    update giao dien partner

--- website-ve-mb/src/App.js
+++ website-ve-mb/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home/HomePage';
 import Payment from './pages/Payment/PaymentPage';
 import FlightSearchingPage from './pages/FlightSearching/FlightSearchingPage';
+import FlightSearchResult from './pages/FlightSearchResult/FlightSearchResult';
 import CreateFlight from './pages/CreateFlight/CreateFlight'; 
 import PartnerLoginPage from './pages/PartnerLoginPage/PartnerLoginPage';
 import PartnerSignup from './pages/PartnerSignupPage/PartnerSignup';
@@ -18,7 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/payment" element={<Payment/>}/>
-        <Route path="/flight-search" element={<FlightSearchingPage/>}/>
+        <Route path="/search-result" element={<FlightSearchResult/>}/>
         <Route path="/create-flight" element={<CreateFlight/>}/>
         <Route path="/partner-login" element={<PartnerLoginPage/>}/>
         <Route path="/partner-signup" element={<PartnerSignup/>}/>
