121f1d65a5a57e30c1e802bbf64b0ee30ef2b957
Thanh Dat <99642423+SturdyStudent@users.noreply.github.com>
2022-05-14 22:21:50 +0700

    Update editFlightModal.js

--- website-ve-mb/src/components/editFlightModal.js
+++ website-ve-mb/src/components/editFlightModal.js
@@ -147,22 +147,12 @@ useEffect(()=>{
 }, [url])
   return (
     <div class="modal-background">
-<<<<<<< HEAD
       <div class='modal-container'>
         <form id='modal-form'>
           <div id='title' style={{"textAlign":"center"}}><h2><b>Cập nhật chuyến bay</b></h2></div>
           <table class="table">
             <tr class="tr">
               <td class="td">
-=======
-      <div class="modal-container">
-        <form>
-          <div class="modal-title"><h2><b>Cập nhật chuyến bay</b></h2></div>
-          
-          <table class="modal-table">
-            <tr>
-              <td class="modal-table-left">
->>>>>>> 74c13f637438ce9e5dd4dbd02a55533ddea9293c
                 <div>Mã chuyến bay:</div>
                 <input type="text" maxLength={10} value={flightParams}></input>
               </td>
