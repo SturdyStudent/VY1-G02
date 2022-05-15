99aead7972fc91e1df472b07ac0a49cac1a61301
SturdyStudent <thanhdat5101@gmail.com>
2022-05-15 09:05:51 +0700

    add port

--- server-database/server.js
+++ server-database/server.js
@@ -10,8 +10,6 @@
  app.use(body_parser.urlencoded({ extended: true }));//dùng khi cần post dữ liệu dạng json
  app.use(cors());
  app.use("/api/partner", partnerRoute);
-//  app.use("/api/seat-class", seatClassRoute);
-// // set port, listen for requests
  const PORT = process.env.PORT || 3001;
  app.listen(PORT, () => {
    console.log(`Server is flying on port ${PORT}.`);
